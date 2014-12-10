6d9acf8d17bb192e9f439c0d43ea009d2cf493ea
Ryan Thompson <service@aiwebsystems.com>
2014-12-10 01:30:33 -0600

    Use PHP instead of YAML. L5 breaks loaders.

---
+++ resources/lang/en/addon.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'name'        => 'Redirects',
+    'description' => 'Redirects module.',
+    'section'     => [
+        'redirects'  => 'Redirects',
+        'extensions' => 'Extensions',
+    ]
+];
---
+++ resources/lang/en/field.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'type' => [
+        'name' => 'Type',
+    ],
+    'from' => [
+        'name' => 'From',
+    ],
+    'to'   => [
+        'name' => 'To',
+    ],
+];
