1ef199f02664faa687aea94abb4265ed923f4e7f
Ryan Thompson <service@aiwebsystems.com>
2015-03-21 10:40:41 -0500

    Nice

--- src/RedirectsModuleServiceProvider.php
+++ src/RedirectsModuleServiceProvider.php
@@ -1,6 +1,6 @@
 <?php namespace Anomaly\RedirectsModule;
 
-use Illuminate\Support\ServiceProvider;
+use Anomaly\Streams\Platform\Addon\AddonServiceProvider;
 
 /**
  * Class RedirectsModuleServiceProvider
@@ -10,17 +10,7 @@ use Illuminate\Support\ServiceProvider;
  * @author        Ryan Thompson <ryan@anomaly.is>
  * @package       Anomaly\RedirectsModule
  */
-class RedirectsModuleServiceProvider extends ServiceProvider
+class RedirectsModuleServiceProvider extends AddonServiceProvider
 {
 
-    /**
-     * Register the service provider.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        $this->app->register('Anomaly\RedirectsModule\Provider\RouteServiceProvider');
-        $this->app->register('Anomaly\RedirectsModule\Redirect\RedirectServiceProvider');
-    }
 }
